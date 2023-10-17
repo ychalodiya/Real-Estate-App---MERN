a7a269b4ac2831f8fc44bad3ddc218326cb9498f
ychalodiya <ychalodiya@gmail.com>
2023-10-17 01:46:48 -0400

    completed seach term functionality to the header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useCookies } from 'react-cookie';
 import { FaSearch } from 'react-icons/fa';
 import { useDispatch, useSelector } from 'react-redux';
@@ -10,6 +10,28 @@ export default function Header() {
 	const dispatch = useDispatch();
 	const { currentUser } = useSelector((state) => state.user);
 	const [cookies, setCookies] = useCookies(['access_token']);
+	const [searchTerm, setSearchTerm] = useState('');
+
+	const changeHandler = (e) => {
+		setSearchTerm(e.target.value);
+	};
+
+	useEffect(() => {
+		const urlParams = new URLSearchParams(window.location.search);
+		const searchTermFromURL = urlParams.get('searchTerm');
+		if (searchTermFromURL) {
+			setSearchTerm(searchTermFromURL);
+		}
+	}, []);
+
+	const submitHandler = (e) => {
+		e.preventDefault();
+		const urlParams = new URLSearchParams(window.location.search);
+		urlParams.set('searchTerm', searchTerm);
+
+		const searchQuery = urlParams.toString();
+		navigate(`/search?${searchQuery}`);
+	};
 
 	const clickHandler = () => {
 		setCookies('access_token', '');
@@ -24,13 +46,20 @@ export default function Header() {
 					<span className="text-slate-500">Maverick</span>
 					<span className="text-slate-700">Estate</span>
 				</h1>
-				<form className="bg-slate-100 p-3 rounded-lg flex items-center">
+				<form
+					onSubmit={submitHandler}
+					className="bg-slate-100 p-3 rounded-lg flex items-center"
+				>
 					<input
 						type="text"
 						placeholder="Search..."
 						className="bg-transparent focus:outline-none w-24 sm:w-64"
+						value={searchTerm}
+						onChange={changeHandler}
 					/>
-					<FaSearch className="text-slate-600" />
+					<button>
+						<FaSearch className="text-slate-600" />
+					</button>
 				</form>
 				<ul className="flex gap-4 ">
 					<li className="hidden sm:inline text-slate-700 hover:underline">
